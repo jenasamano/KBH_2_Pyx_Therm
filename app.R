2bad1b821b1b378f0cd92d527c6103d9bee31128
jenasamano <jsamano@vols.utk.edu>
2021-11-17 12:49:48 -0500

    added tabs,condensed displayed table, added image but there are still issues with this, added section for instructions, added U/I drop down to select types of plots,added lm line smoothing to the plots

--- app.R
+++ app.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(flexdashboard)
 library(tidyverse)
+library(janitor)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -21,52 +22,115 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("Pressure", "Pressure in kilobars:", min = 1, max = 20, value = 1),
-            fileInput("uploaded_data", "Upload Electron Microprobe data", accept = ".csv")
+            fileInput("uploaded_data", "Upload Electron Microprobe data", accept = ".csv"),
+            selectInput("x_axis", "X-axis data", choices = c("point_location_CPX",
+                                              "point_location_OPX",
+                                              "Temp_Celsius",
+                                              "X_Fe_OPX",
+                                              "X_Fe_CPX"),
+                        selected = "Temp_Celsius"),
+            
+            selectInput("y_axis", "y-axis data", choices = c("point_location_CPX",
+                                              "point_location_OPX",
+                                              "Temp_Celsius",
+                                              "X_Fe_OPX",
+                                              "X_Fe_CPX"),
+                        selected = "point_location_CPX"),
+            
+            radioButtons("full_table", "Show Full Table of Calulations",
+                         c("yes"= "yes",
+                           "no" = "no"), selected = "no")
         ),
 
-        # Show a plot of the generated distribution
-        mainPanel("results_go_here")
+        # Show a table and plot of the generated distribution
+        mainPanel(
+        tabsetPanel(
+          id = "tabs",
+          tabPanel(
+            "Data and Plots",
+            
+            tableOutput(outputId = "samp_data_prototypeT"),
+            
+            plotOutput(outputId = "location_Temp")
+            
+          ),
+        tabPanel("Instructions", 
+                 p("instructions go here in this format"),
+                 img(src = "images/test_image.png", height = 100, width = 100))   
         )
+        ))
     )
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
+server <- function(input, output){
+  
     output$contents <-renderTable({
         uploaded_file <- input$uploaded_data
         if(is.null(uploaded_file)) return(NULL)
-        read.csv(uploaded_file$datapath)
+        read.csv(uploaded_file$datapath, header = TRUE)
         
     })
     
-    output$therm_output <-renderTable({
-        samp_data_prototypeT <- input$uploaded_data
+    output$samp_data_prototypeT <-renderTable({
+      
+        uploaded_file <- input$uploaded_data
         
-        samp_data_prototypeT <- mutate(samp_data_prototypeT,X_Fe_CPX=Nb_ions_Fe_CPX/(Nb_ions_Fe_CPX+Nb_ions_Mg_CPX)) 
+        if(is.null(input$uploaded_data)) return(NULL)
+        else{
+          samp_data_prototypeT <- read.csv(uploaded_file$datapath, header = TRUE) 
+          names(samp_data_prototypeT)[str_detect(names(samp_data_prototypeT),"point_location_CPX")] <- "point_location_CPX"
+       samp_data_prototypeT <- mutate(samp_data_prototypeT,X_Fe_CPX = Nb_ions_Fe_CPX/(Nb_ions_Fe_CPX + Nb_ions_Mg_CPX))
         samp_data_prototypeT
-        
-        Pressure <- 10
-        
+
+        Pressure <- input$Pressure
+
         samp_data_prototypeT <- mutate(samp_data_prototypeT,
-                                       X_Fe_OPX = Nb_ions_Fe_OPX/(Nb_ions_Fe_OPX+Nb_ions_Mg_OPX),
+                                       X_Fe_OPX = Nb_ions_Fe_OPX/(Nb_ions_Fe_OPX + Nb_ions_Mg_OPX),
                                        Ca_star_CPX = Nb_ions_Ca_CPX/(1-Nb_ions_Na_CPX),
                                        Ca_star_OPX = Nb_ions_Ca_OPX/(1-Nb_ions_Na_OPX),
                                        K_sub_D = (1-Ca_star_CPX)/(1-Ca_star_OPX),
                                        onehundredtwentysix_times_X_Fe_CPX=126.3*X_Fe_CPX,
                                        plustwentyfour = onehundredtwentysix_times_X_Fe_CPX+24.9,
                                        times_pressure = plustwentyfour*Pressure,
-                                       T_bacon_numerator = 23664+times_pressure,
+                                       T_bacon_numerator = 23664 + times_pressure,
                                        ln_K_sub_D = log(K_sub_D),
                                        ln_K_sub_D_SQRD = ln_K_sub_D^2,
-                                       T_bacon_denominator = 13.38+ln_K_sub_D_SQRD+(11.59*X_Fe_OPX),
+                                       T_bacon_denominator = 13.38 + ln_K_sub_D_SQRD + (11.59 * X_Fe_OPX),
                                        Temp_Kelvins = T_bacon_numerator/T_bacon_denominator,
                                        Temp_Celsius = Temp_Kelvins-273.15)
         
-        renderTable({ samp_data_prototypeT })
-      
-        
+        display_table <- select(samp_data_prototypeT,
+                                point_location_CPX,
+                                point_location_OPX,
+                                Temp_Celsius,
+                                Temp_Kelvins,
+                                X_Fe_CPX,
+                                X_Fe_OPX)
+        if(input$full_table=="yes") samp_data_prototypeT
+        else display_table
+        }    
     })
+    output$location_Temp <- renderPlot({
+      if(is.null(input$uploaded_data)) return(NULL)
+      else{
+        
+        r_x_axis <- reactive({
+          r_x_axis <- as.symbol(input$x_axis)
+         
+        })
+        
+        r_y_axis <- reactive({
+          r_y_axis <- as.symbol(input$y_axis)
+          
+        })
+        
+        ggplot(samp_data_prototypeT,aes(x= !!r_x_axis(), y = !!r_y_axis()))+
+      geom_point()+
+          geom_smooth(method="lm")
+      
+    }})
+    
 }
 
 # Run the application 
