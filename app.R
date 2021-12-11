b1cd05ef13da9c01cde02a9179da4db9c5d59057
Andrew Steen <andrew.decker.steen@gmail.com>
2021-12-11 13:49:13 -0500

    Move instructions to their own markdown file.

--- app.R
+++ app.R
@@ -54,34 +54,8 @@ ui <- fluidPage(
             plotOutput(outputId = "location_Temp")
             
           ),
-        tabPanel("Instructions", 
-                 p("It is vital your uploaded table is in the same format as the table shown here. 
-                   Label you columns exactly as shown. 
-                   The calculations will not execute properly unless you do this step.
-                   For your convenience the column names are listed here: 
-                   1. point_location_CPX
-                   2. point_location_OPX
-                   3. point_number_CPX
-                   4. point_number_OPX
-                   5. Nb_ions_Mg_CPX
-                   6 Nb_ions_Mg_OPX
-                   7. Nb_ions_Fe_CPX
-                   8 Nb_ions_Fe_OPX
-                   9. Nb_ions_Ca_CPX
-                   10. Nb_ions_Ca_OPX
-                   11. Nb_ions_Na_CPX
-                   12. Nb_ions_Na_OPX
-                   upload you table in a csv format.
-                   
-                   You will need to clean and arrange your data from the microbrobe.
-                   
-                   your data number points for the microprobe are usually in the form: 2/1.
-                   R will not recognize this form and you will need to re-number
-                   them to a whole integer.
-                   
-                   You must use the Nb of ions from the microprobe data.
-                   This code assumes you have used the correct analysis tool and will
-                   treat values as correct."),
+        tabPanel("Instructions", # I'd probably put this text in a separate function. Note also that it isn't using markdown
+                 includeMarkdown("instructions.md"),
                  
                  img(src = "proper_format.jpg", height = 900, width = 1500))   
         )
